e41e34a3d1e944f4dab8303248756973c4e0ad55
CHIZI-0618 <51962278+CHIZI-0618@users.noreply.github.com>
2026-02-19 14:53:42 +0800

    chore: bump version to v26.02.19

--- tproxy.sh
+++ tproxy.sh
@@ -2,7 +2,7 @@
 
 readonly SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd -P)"
 # Version (use YY.MM.DD format)
-readonly SCRIPT_VERSION="v26.02.18.1"
+readonly SCRIPT_VERSION="v26.02.19"
 
 # Configuration (modify as needed)
 
